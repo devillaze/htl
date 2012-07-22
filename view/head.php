8b997ea6d0141f79adb6cda0cc4e5b001c5fcfe0
devillaze <devillaze@yahoo.com>
2012-07-22 12:04:41 +0800

    js

---
+++ view/head.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> HTL - <?php echo $title; ?></title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="js/fancy/jquery.fancybox.css">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel-3.0.6.pack.js"></script>
+<script type="text/javascript" src="js/fancy/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/fancy/jquery.fancybox.js"></script>
+</head>
+<body>
